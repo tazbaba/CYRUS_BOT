898e0728901e96eefa5bfe422f1430b8918deabc
tazbaba <144560150+tazbaba@users.noreply.github.com>
2025-07-26 08:01:19 +0600

    Update bot.js

--- Rudra/commands/bot.js
+++ Rudra/commands/bot.js
@@ -22,7 +22,7 @@ module.exports.handleEvent = async function({ api, event, args, Threads, Users }
    mess = "{name}"
   if (event.body.indexOf("Bot") == 0 || (event.body.indexOf("bot") == 0)) {
     var msg = {
-      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : 𝗔𝗿𝘂𝗻 𝗞𝘂𝗺𝗮𝗿🌹 `
+      body: `🔶${name}🔶,  \n\n『\n   ${rand} 』\n\n❤️𝙲𝚛𝚎𝚍𝚒𝚝𝚜 : CYB3R_W0LF🌹 `
     }
     return api.sendMessage(msg, threadID, messageID);
   };
